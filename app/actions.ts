6f49abfa81dec379ca559fc949139dfe741db4a2
shashwat558 <jainshashwat528@gmail.com>
2025-05-12 02:03:02 +0530

    added google auth

---
+++ app/actions.ts
@@ -0,0 +1,26 @@
+"use server"
+
+import { createClientServer } from "@/lib/utils/supabase/server"
+import { redirect } from "next/navigation";
+
+
+const signInWIth = provider => async () => {
+    const supabase = await createClientServer();
+    const callback_url = `${process.env.SITE_URL}/auth/callback`;
+
+    const {data, error} = await supabase.auth.signInWithOAuth({
+        provider,
+        options: {
+            redirectTo: callback_url
+        }
+    })
+    console.log(data);
+    if(error){
+        console.log(error)
+    }
+
+    redirect(data.url ?? "")
+
+}
+
+export const signInWIthGoogle = signInWIth('google');
